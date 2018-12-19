fcb022f38e4b487b49020fd5d6f1463737cbcc0b
Blackbourn, Joshua R <148062@s020rmc148-15.local>
2018-12-19 14:22:20 -0600

    hey

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
 
 }
 
