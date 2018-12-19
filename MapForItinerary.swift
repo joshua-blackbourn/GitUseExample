644eed24b70bb4b986932256702c1175f144fc5a
Blackbourn, Joshua R <148062@s020rmc148-15.local>
2018-12-19 14:25:46 -0600

    hey

---
+++ MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Blackbourn, Joshua R on 12/19/18.
+//  Copyright © 2018 Blackbourn, Joshua R. All rights reserved.
+//
+
+import Foundation
