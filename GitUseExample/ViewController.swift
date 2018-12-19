a7052f3e7dc68cdd67e6dc4c6c1ba83421c00afe
Blackbourn, Joshua R <148062@s020rmc148-15.local>
2018-12-19 14:05:45 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Blackbourn, Joshua R on 12/19/18.
+//  Copyright © 2018 Blackbourn, Joshua R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
